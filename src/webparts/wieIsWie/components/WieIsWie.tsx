2c90ccd8f65b431809808eb20bbec0182623c4d2
yellowarrownl <help@yellowarrow.nl>
2018-11-09 14:02:42 +0100

    Upgrade/downgrade survival

--- src/webparts/wieIsWie/components/WieIsWie.tsx
+++ src/webparts/wieIsWie/components/WieIsWie.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { MSGraphClient } from '@microsoft/sp-http';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import { UserTeamRole } from '@microsoft/teams-js';
-//import { IPersonaProps, IPersonaSharedProps, Persona, PersonaSize, PersonaPresence } from 'office-ui-fabric-react/lib/Persona';
+import { IPersonaProps, Persona, PersonaSize, PersonaPresence } from 'office-ui-fabric-react/lib/Persona';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
 export interface IUserProps {
   graphClient: MSGraphClient;
@@ -21,21 +22,7 @@ export default class WieIsWie extends React.Component<IUserProps, IUsersState> {
       users: []
     };
   }
-
-  /* public componentDidMount(): void {
-    console.log("Hier roepen wij de functie Graph aan");
-    this.props.graphClient
-    .api('/users')
-    .get((error:any, res: any, rawResponse?: any) => {
-      console.log(res);
-      console.log(error);
-      // Passes the information to a constructor to set the state
-      const userList:MicrosoftGraph.User[] = res.value;
-      this.setState({users: userList});
-      console.log(this.state);
-    });
-  } */
-    
+   
   public componentDidMount(): void { 
     this.props.graphClient
     .api('/users')
@@ -54,23 +41,16 @@ export default class WieIsWie extends React.Component<IUserProps, IUsersState> {
       element.displayName = ""
     }) */
     console.log("Dit is een test");
+    console.log(this.state.users);
     return (
-      <div>
-        <div>
-          <form action="">
-            Filter: <input type="text"/>
-            <button  type="button"  onClick={(e) => this.klikOpFilter(e)}>Filter lijst</button>
-          </form>
-        </div>
-        <ul> 
-          {this.state.users.map((user) =>
-              <li>
-                {user.displayName}
-                <button onClick={(e) => this.klikOpUser(e, user.displayName)}> Verwijder </button>
-              </li> 
-            )}
-        </ul>
-
+      <div>{this.state.users.map((user)=>
+        <Persona 
+        {...user} 
+        imageUrl={'/_layouts/15/userphoto.aspx?accountname='+ user.mail +'&size=L'} 
+        primaryText={user.displayName}
+        secondaryText= {user.mail}  
+        />
+        )}
       </div>
     );
   }
@@ -92,3 +72,44 @@ export default class WieIsWie extends React.Component<IUserProps, IUsersState> {
 
 }
 
+/*
+<div>
+    <div>
+    <form action="">
+      Filter: <input type="text"/>
+      <button  type="button"  onClick={(e) => this.klikOpFilter(e)}>Filter lijst</button>
+    </form>
+  </div>
+  <ul> 
+    {this.state.users.map((user) =>
+      <li>
+            <Persona {...user} />
+          {user.displayName}
+          <button onClick={(e) => this.klikOpUser(e, user.displayName)}> Verwijder </button>
+        </li>
+      )}
+  </ul>
+</div>
+
+
+
+
+
+
+
+      <div className="ms-SearchBoxSmallExample">
+        <SearchBox
+          placeholder="Search"
+          onEscape={ev => {
+            console.log('Custom onEscape Called');
+          }}
+          onClear={ev => {
+            console.log('Custom onClear Called');
+          }}
+          onChange={newValue => console.log('SearchBox onChange fired: ' + newValue)}
+          onSearch={newValue => console.log('SearchBox onSearch fired: ' + newValue)}
+          onFocus={() => console.log('onFocus called')}
+          onBlur={() => console.log('onBlur called')}
+        />
+      </div>
+*/
